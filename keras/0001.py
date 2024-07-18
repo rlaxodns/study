f0208bf39ebf2b08235817f623940e05d5ec5792
rlaxodns <gjwlsghl123@naver.com>
2024-07-18 19:54:35 +0900

    .

---
+++ keras/0001.py
@@ -0,0 +1,15 @@
+adasda
+car = {
+  'brand': 'Ford',
+  'model': 'Mustang',
+  'year': 1964
+}
+x = car.setdefault('color', 'white')
+print(x)assert
+car = {
+  'brand': 'Ford',
+  'model': 'Mustang',
+  'year': 1964
+}
+x = car.setdefault('color', 'white')
+print(x)def
