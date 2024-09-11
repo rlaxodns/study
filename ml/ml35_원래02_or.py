97bffca200b85df7d0aefdde8b697594405b401c
rlaxodns <gjwlsghl123@naver.com>
2024-09-11 10:56:41 +0900

    .

---
+++ ml/ml35_원래02_or.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+
+#1. 데이터
+x_data = np.array([[0,0],[0,1],[1,0],[1,1]])
+y_data = np.array([0,1,1,1])
+# print(x_data.shape, y_data.shape) #(4, 2) (4,)
+
+#2. 모델
+model = LinearSVC()
+# model = Perceptron()
+
+#3. 훈련
+model.fit(x_data, y_data)
+
+#4. 평가 및 예측
+acc= model.score(x_data, y_data)
+print(acc)
+
+y_pre = model.predict(x_data)
+acc2 = accuracy_score(y_data, y_pre)
+print(acc2)
+
+print('========================================')
+print(y_data)
+print(y_pre)
+
+# 잘맞음
---
+++ ml/ml35_원래03_xor.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+
+#1. 데이터
+x_data = np.array([[0,0],[0,1],[1,0],[1,1]])
+y_data = np.array([0,1,1,0])
+# print(x_data.shape, y_data.shape) #(4, 2) (4,)
+
+#2. 모델
+# model = LinearSVC()
+model = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+
+#3. 훈련
+model.fit(x_data, y_data)
+
+#4. 평가 및 예측
+acc= model.score(x_data, y_data)
+print(acc)
+
+y_pre = model.predict(x_data)
+acc2 = accuracy_score(y_data, y_pre)
+print(acc2)
+
+print('========================================')
+print(y_data)
+print(y_pre)
+
+# 반만 맞음
